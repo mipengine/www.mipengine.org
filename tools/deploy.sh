4ac7e6138cfdf58369b60f83fc49a5ee29a31022
xuexb <fe.xiaowu@gmail.com>
2017-11-07 21:18:15 +0800

    fix: 修复编译脚本目录

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,34 +1,32 @@
 #!/bin/bash
 
-dir=$(cd "$(dirname ${0})"; pwd)
-
-if [ ! -n "$TRAVIS_TAG" ]; then 
+if [ ! -n "$TRAVIS_TAG" ]; then
     echo "CI tag 为空"
     exit 1
 fi
 
-if [ ! -n "$ACCESS_KEY" ]; then 
+if [ ! -n "$ACCESS_KEY" ]; then
     echo "ACCESS_KEY 没有配置"
     exit 1
 fi
 
-if [ ! -n "$SECRET_ACCESS_KEY" ]; then 
+if [ ! -n "$SECRET_ACCESS_KEY" ]; then
     echo "SECRET_ACCESS_KEY 没有配置"
     exit 1
 fi
 
-if [ ! -n "$GIT_URL" ]; then 
+if [ ! -n "$GIT_URL" ]; then
     echo "GIT_URL 没有配置"
     exit 1
 fi
 
-rm -rf tmp && mkdir tmp && cd tmp
-
 git clone "https://$ACCESS_KEY:$SECRET_ACCESS_KEY@$GIT_URL" bae
-cp -r $dir/dist/* bae
+cp -r dist/* bae/
 cd bae
+
 git add .
 git commit -m "release: $TRAVIS_TAG"
 git push
 
-cd $dir && rm -rf tmp
+cd ../
+rm -rf bae
