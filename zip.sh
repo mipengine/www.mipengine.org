dd0adedb7ac0aaa331da1c96e3311510ec30f735
tanglei02 <tanglei02@baidu.com>
2019-09-19 12:44:01 +0800

    修复打包脚本

--- zip.sh
+++ zip.sh
@@ -1,7 +1,13 @@
 
 set +e
 
-npm run build
+# npm run build
+
+rm -rf api codelabs guide components extensions img assets/img mip docs tmp index.html v2 ui contribute about news
+
+node node_modules/webpack/bin/webpack.js --config src/style/webpack.config.js
+
+node src/index.js
 
 # 移除原zip包
 echo "------zip to v2-----"
